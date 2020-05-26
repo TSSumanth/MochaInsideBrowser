f731cde1c0621738540b0b9033b569bbb49b6f79
Sumanth <tssumanth18@gmail.com>
2020-05-27 01:28:09 +0530

    initial commit

---
+++ tests/test.spec.js
@@ -0,0 +1,11 @@
+// var moduleName = './Main/calculatorApp.js'
+// require([moduleName], function(fooModule){
+//     // do something with fooModule
+// })
+describe('test descript',function(){
+    console.log("test describe executed")
+    it('test it',function(){
+        console.log("test it executed")
+        console.log(add2(2,4))
+    })
+})
